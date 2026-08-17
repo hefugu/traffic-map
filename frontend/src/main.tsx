a048d15cf202a4d09dbe05d6fc011d259f0b665a
helo <132366128+hefugu@users.noreply.github.com>
2026-08-17 23:21:17 +0900

    Serve navigation marker icons locally
    * Vendor Leaflet color marker assets
    
    * Use local marker assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import L from 'leaflet'
 import App from './App'
 import 'leaflet/dist/leaflet.css'
+
+const localMarkerUrls: Record<string, string> = {
+  'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png': '/markers/marker-icon-green.png',
+  'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png': '/markers/marker-icon-orange.png',
+}
+
+const originalCreateIcon = L.Icon.prototype.createIcon
+L.Icon.prototype.createIcon = function (oldIcon?: HTMLElement) {
+  const iconUrl = this.options.iconUrl
+  if (iconUrl) {
+    const localMarkerUrl = localMarkerUrls[iconUrl]
+    if (localMarkerUrl) this.options.iconUrl = localMarkerUrl
+  }
+  return originalCreateIcon.call(this, oldIcon)
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
